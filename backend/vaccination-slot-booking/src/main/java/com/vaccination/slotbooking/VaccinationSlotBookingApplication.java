e924f78f87458087ae6b4463be6185220623010f
jpugazholi <jpugazholi@gmail.com>
2026-08-16 12:17:19 +0530

    feat(backend): initialize spring boot project

---
+++ backend/vaccination-slot-booking/src/main/java/com/vaccination/slotbooking/VaccinationSlotBookingApplication.java
@@ -0,0 +1,13 @@
+package com.vaccination.slotbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationSlotBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationSlotBookingApplication.class, args);
+	}
+
+}
